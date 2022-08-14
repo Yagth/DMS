906160262337a649344904239d35cb78a4bf20b8
Yagth <yabuman46@gmail.com>
2022-08-13 18:43:44 -0700

    Created the ExtendDormStayForm gui design

---
+++ src/GUIClasses/StudentViews/RequestForDormitory.java
@@ -0,0 +1,19 @@
+package GUIClasses.StudentViews;
+
+import javax.swing.*;
+
+public class RequestForDormitory {
+    private JLabel titleLabel;
+    private JTextField textField1;
+    private JLabel addressLabel;
+    private JPanel inputPanel;
+    private JLabel subcityLabel;
+    private JTextField textField2;
+    private JLabel woredaLabel;
+    private JTextPane descriptionPane;
+    private JScrollPane descriptionSP;
+    private JLabel descriptionLabel;
+    private JPanel mainPanel;
+    private JPanel titlePanel;
+    private JButton submitButton;
+}
