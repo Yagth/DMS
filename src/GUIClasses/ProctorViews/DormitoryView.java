4a807a35d110f9530fa6f50162d6b5c8128188b2
Yagth <yabuman46@gmail.com>
2022-08-21 08:10:24 -0700

    Edited the DormitoryView

--- src/GUIClasses/ProctorViews/DormitoryView.java
+++ src/GUIClasses/ProctorViews/DormitoryView.java
@@ -1,11 +1,12 @@
 package GUIClasses.ProctorViews;
 
+import BasicClasses.Enums.SizeOfMajorClasses;
 import GUIClasses.Interfaces.Views;
 
 import javax.swing.*;
 
 public class DormitoryView extends JFrame implements Views {
-    private JFormattedTextField searchDormTA;
+    private JFormattedTextField searchBuildingNoTA;
     private JButton searchButton;
     private JButton filterButton;
     private JPanel headingPanel;
@@ -14,12 +15,9 @@ public class DormitoryView extends JFrame implements Views {
     private JLabel totalSpaceNo;
     private JLabel emptyDormNoLabel;
     private JPanel totalDormInfoPanel;
-    private JButton allocateButton;
-    private JButton changeButton;
-    private JButton deallocateButton;
     private JButton backButton;
-    private JButton nextButton;
-    private JButton previousButton;
+    private JButton nextPageButton;
+    private JButton previousPageButton;
     private JLabel dormLocation;
     private JLabel noStudentsLabel;
     private JPanel listOfDormPanel;
@@ -31,8 +29,12 @@ public class DormitoryView extends JFrame implements Views {
     private JPanel numberOfStudentsPanel;
     private JPanel dormListPanel;
     private JComboBox filterList;
-    private static final int WIDTH = 650;
-    private static final int HEIGHT = 700;
+    private JLabel BNOLabel;
+    private JFormattedTextField seatchRoomNoTA;
+    private JTextField yearTA;
+    private JLabel RNOLabel;
+    private static final int WIDTH = SizeOfMajorClasses.WIDTH.getSize();
+    private static final int HEIGHT = SizeOfMajorClasses.HEIGHT.getSize();
 
     public DormitoryView(){
         setUpGUi();
@@ -52,5 +54,22 @@ public class DormitoryView extends JFrame implements Views {
 
         ImageIcon searchButtonIcon = new ImageIcon("Icons/SearchIcon.png");
         searchButton.setIcon(searchButtonIcon);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu actions = new JMenu("Actions");
+
+        JMenuItem allocate = new JMenuItem("Allocate Dorms");
+        JMenuItem deallocate = new JMenuItem("Deallocate all dorms");
+        JMenuItem change = new JMenuItem("Change");
+
+        actions.add(allocate);
+        actions.add(deallocate);
+        actions.add(change);
+
+        menuBar.add(actions);
+
+        this.setJMenuBar(menuBar);
+
     }
 }
