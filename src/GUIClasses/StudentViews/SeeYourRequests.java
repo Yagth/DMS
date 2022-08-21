883f6b59099c02cb64564602a3437f8717a44ce2
Yagth <yabuman46@gmail.com>
2022-08-21 02:06:17 -0700

    Edited the substring part out for testing

--- src/GUIClasses/StudentViews/SeeYourRequests.java
+++ src/GUIClasses/StudentViews/SeeYourRequests.java
@@ -86,9 +86,9 @@ public class SeeYourRequests extends JFrame implements Views, TableViews {
                 tmp.add(reports.getInt("reportId"));
                 tmp.add(reports.getString("ReportType"));
                 tmp.add(reports.getString("ReportedDate"));
-                String temp = reports.getString("description");
-                temp = temp.substring(0,temp.length()/5)+"...";
-                tmp.add(temp);
+//                String temp = reports.getString("description");
+//                temp = temp.substring(0,temp.length()/5)+"...";
+                tmp.add(reports.getString("description"));
                 addDataToTable(tmp);
             }
         }
