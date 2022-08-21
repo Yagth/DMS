8b6c61a163904881df26aab2aa3f5a6e720b947d
Yagth <yabuman46@gmail.com>
2022-08-21 09:39:24 -0700

    Set up Icons and changed buttons with labels

--- src/GUIClasses/ProctorViews/DormitoryView.java
+++ src/GUIClasses/ProctorViews/DormitoryView.java
@@ -15,9 +15,6 @@ public class DormitoryView extends JFrame implements Views {
     private JLabel totalSpaceNo;
     private JLabel emptyDormNoLabel;
     private JPanel totalDormInfoPanel;
-    private JButton backButton;
-    private JButton nextPageButton;
-    private JButton previousPageButton;
     private JLabel dormLocation;
     private JLabel noStudentsLabel;
     private JPanel listOfDormPanel;
@@ -33,6 +30,9 @@ public class DormitoryView extends JFrame implements Views {
     private JFormattedTextField seatchRoomNoTA;
     private JTextField yearTA;
     private JLabel RNOLabel;
+    private JLabel backLabel;
+    private JLabel previousPageLabel;
+    private JLabel nextPageLabel;
     private static final int WIDTH = SizeOfMajorClasses.WIDTH.getSize();
     private static final int HEIGHT = SizeOfMajorClasses.HEIGHT.getSize();
 
@@ -55,11 +55,13 @@ public class DormitoryView extends JFrame implements Views {
         ImageIcon searchButtonIcon = new ImageIcon("Icons/SearchIcon.png");
         searchButton.setIcon(searchButtonIcon);
         ImageIcon backButtonIcon = new ImageIcon("Icons/backIcon.png");
-        backButton.setIcon(backButtonIcon);
-        ImageIcon nextButtonIcon = new ImageIcon("Icons/nextIcon.png");
-        nextPageButton.setIcon(nextButtonIcon);
-        ImageIcon previousButtonIcon = new ImageIcon("Icons/previousIcon.png");
-        previousPageButton.setIcon(previousButtonIcon);
+        backLabel.setIcon(backButtonIcon);
+        ImageIcon nextButtonIcon = new ImageIcon("Icons/nextIcon(3).png");
+        nextPageLabel.setIcon(nextButtonIcon);
+        ImageIcon previousButtonIcon = new ImageIcon("Icons/previousIcon-20x20.png");
+        previousPageLabel.setIcon(previousButtonIcon);
+        ImageIcon filterButtonIcon = new ImageIcon("Icons/FilterIcon.png");
+        filterButton.setIcon(filterButtonIcon);
 
         JMenuBar menuBar = new JMenuBar();
 
