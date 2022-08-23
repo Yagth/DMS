d8f69f9d1edef48404bce85c7ff050a3b448bc50
Yagth <yabuman46@gmail.com>
2022-08-23 10:11:19 -0700

    Added some backend for the change dorm view

--- src/GUIClasses/ProctorViews/ChangeDormView.java
+++ src/GUIClasses/ProctorViews/ChangeDormView.java
@@ -1,8 +1,11 @@
 package GUIClasses.ProctorViews;
 
+import BasicClasses.Persons.Proctor;
+import GUIClasses.Interfaces.Views;
+
 import javax.swing.*;
 
-public class ChangeDormView {
+public class ChangeDormView extends JFrame implements Views {
     private JPanel mainPanel;
     private JComboBox conditions;
     private JFormattedTextField fromBuildingNoTF;
@@ -17,4 +20,51 @@ public class ChangeDormView {
     private JButton backButton;
     private JButton changeButton;
     private JLabel numberOfStudentsL;
+    private Proctor proctor;
+    private DormitoryView parentComponent;
+    public ChangeDormView(Proctor proctor, DormitoryView parentComponent){
+        this.proctor = proctor;
+        this.parentComponent = parentComponent;
+        setUpGUi();
+    }
+    public ChangeDormView(){
+        this(null,null);
+    }
+
+    public String getSelectedCondition(){
+        return (String) conditions.getSelectedItem();
+    }
+
+    public String getDormNo(){
+        return fromRoomNoTF.getText();
+    }
+    public String getBuildingNo(){
+        return fromBuildingNoTF.getText();
+    }
+    public String getDestinationBuildingNo(){
+        return toBuildingNoTF.getText();
+    }
+    public String getDestinationRoomNo(){
+        return toRoomNoTF.getText();
+    }
+    public int getNoOfStudent(){
+        //This function will return the number of
+        // students that satisfy the condition selected in the condition's comboBox.
+        return 0;
+    }
+    public void updateViewOnCondition(){
+        /*
+        This method will set the invisible components to visible and
+        vice versa by checking the selected condition in the comboBox.
+         */
+    }
+
+    @Override
+    public void setUpGUi() {
+        this.setTitle("Change Dormitory");
+        this.setSize(500,300);
+        this.setContentPane(mainPanel);
+        this.setLocationRelativeTo(parentComponent);
+        this.setVisible(true);
+    }
 }
--- src/GUIClasses/ProctorViews/Test.java
+++ src/GUIClasses/ProctorViews/Test.java
@@ -4,6 +4,6 @@ import BasicClasses.Rooms.Dormitory;
 import GUIClasses.LoginPage;
 
 public class Test {
-    public static void main(String[] args) {new DormitoryDetailView(new Dormitory("100","306"),null,null);
+    public static void main(String[] args) {new ChangeDormView();
     }
 }
