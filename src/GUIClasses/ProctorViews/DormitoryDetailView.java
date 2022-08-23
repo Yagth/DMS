7c4dc68a8ed93b40d56b9d39dd27973c9026bcae
Yagth <yabuman46@gmail.com>
2022-08-23 05:43:25 -0700

    added a DormitoryDetailView

---
+++ src/GUIClasses/ProctorViews/DormitoryDetailView.java
@@ -0,0 +1,18 @@
+package GUIClasses.ProctorViews;
+
+import javax.swing.*;
+
+public class DormitoryDetailView {
+    private JPanel mainPanel;
+    private JPanel dormInfoPanel;
+    private JPanel studentListPanel;
+    private JLabel buildingNoL;
+    private JLabel bedNoL;
+    private JLabel lockerNoL;
+    private JLabel tableNoL;
+    private JLabel chiarsNoL;
+    private JLabel keyHolderL;
+    private JScrollPane studentListSP;
+    private JTable studentList;
+    private JButton backButton;
+}
