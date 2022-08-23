c61dbeadb04a3a32678297480d5200e08ab0bd51
Yagth <yabuman46@gmail.com>
2022-08-23 11:17:22 -0700

    Added StudentView

---
+++ src/GUIClasses/ProctorViews/StudentView.java
@@ -0,0 +1,18 @@
+package GUIClasses.ProctorViews;
+
+import javax.swing.*;
+
+public class StudentView {
+    private JPanel mainPanel;
+    private JPanel upperPanel;
+    private JFormattedTextField searchTF;
+    private JButton searchButton;
+    private JComboBox filterCondition;
+    private JButton filterButton;
+    private JPanel studentListPanel;
+    private JTable studentListTabel;
+    private JPanel buttonPanel;
+    private JButton nextButton;
+    private JButton prevButton;
+    private JButton backButton;
+}
