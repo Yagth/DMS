a9fde84c109d3a7f1cfc7df0b0e0e03eb334edfb
Yagth <yabuman46@gmail.com>
2022-08-23 05:12:57 -0700

    Added a DeallocateDormView

--- src/GUIClasses/ProctorViews/DeallocateDormView.java
+++ src/GUIClasses/ProctorViews/DeallocateDormView.java
@@ -1,4 +1,12 @@
 package GUIClasses.ProctorViews;
 
+import javax.swing.*;
+
 public class DeallocateDormView {
+    private JFormattedTextField frombuildingNoTF;
+    private JComboBox conditions;
+    private JPanel mainPanel;
+    private JButton backButton;
+    private JButton deallocateButton;
+    private JLabel numberOfStudentsL;
 }
