3fc8e6e22378764ad0d1d999af48d2ab77288ca0
Yagth <yabuman46@gmail.com>
2022-08-24 23:38:13 -0700

    Added the table setup code

--- src/GUIClasses/ProctorViews/StudentDetailView.java
+++ src/GUIClasses/ProctorViews/StudentDetailView.java
@@ -6,7 +6,9 @@ import GUIClasses.Interfaces.TableViews;
 import GUIClasses.Interfaces.Views;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.View;
+import java.util.Vector;
 
 public class StudentDetailView extends JFrame implements Views, TableViews {
     private JPanel mainPanel;
@@ -25,6 +27,8 @@ public class StudentDetailView extends JFrame implements Views, TableViews {
     private JButton backButton;
     private Student student;
     private StudentView parentComponent;
+    private Vector<Vector<Object>> tableData;
+    private boolean readStatus;
     private static final int WIDTH = SizeOfMajorClasses.WIDTH.getSize();
     private static final int HEIGHT = SizeOfMajorClasses.HEIGHT.getSize();
 
@@ -35,20 +39,52 @@ public class StudentDetailView extends JFrame implements Views, TableViews {
 
     }
     public StudentDetailView(){this(null,null);}//This constructor is only for debugging purposes.
+    public void displayStudentInfo(){
+        /*
+        This method will display all the student info in the labels except for the emergency contact info
+        which will need to be displayed in the table since it contains more field than just names.
+         */
+    }
 
+    public void loadEmergencyContacts(){
+        /*
+        This method will load every emergency contacts from the database and add them in the tableData
+        vector accordingly.
+         */
+    }
+
+    public void displayReadStatus(boolean readStatus){
+        if(!readStatus)
+            JOptionPane.showMessageDialog(this,"Couldn't read the emergency contacts due to connection error"
+                    ,"Reading Error",JOptionPane.ERROR_MESSAGE);
+    }
     @Override
     public void setUpTable() {
-
+        Vector<String> titles = new Vector<>();
+        tableData = new Vector<>();
+        titles.add("Name");
+        titles.add("Phone Number");
+        titles.add("Place of Origin");
+        titles.add("Relation to student");
+        emergencyContactList.setModel(new DefaultTableModel(tableData,titles));
+        emergencyContactList.setDefaultEditor(Object.class,null); // To make the table non editable.
+        loadEmergencyContacts();
+        readStatus = !(tableData == null); //tableData will be null if the read isn't successful making readStatus false.
+        displayReadStatus(readStatus);
+        refreshTable();
     }
 
     @Override
     public void addDataToTable(Object object) {
-
+        /*
+        No need to implement this method.
+         */
     }
 
     @Override
     public void refreshTable() {
-
+        DefaultTableModel tableModel = (DefaultTableModel) emergencyContactList.getModel();
+        tableModel.fireTableDataChanged();
     }
 
     @Override
@@ -57,6 +93,7 @@ public class StudentDetailView extends JFrame implements Views, TableViews {
         this.setContentPane(mainPanel);
         this.setSize(WIDTH,HEIGHT);
         this.setLocationRelativeTo(null);
+        setUpTable();
         this.setVisible(true);
     }
 }
