0d128423b09386d33a1847e1e1ed50ed5a7afee4
Yagth <yabuman46@gmail.com>
2022-08-24 23:16:26 -0700

    Added the studentDetailView

--- src/GUIClasses/ProctorViews/StudentDetailView.java
+++ src/GUIClasses/ProctorViews/StudentDetailView.java
@@ -4,4 +4,17 @@ import javax.swing.*;
 
 public class StudentDetailView {
     private JPanel mainPanel;
+    private JPanel studetnInfoPanel;
+    private JPanel buttonPanel;
+    private JPanel emergencyContactPanel;
+    private JTable emergencyContactList;
+    private JLabel studentNameL;
+    private JLabel idL;
+    private JLabel buildingNumberL;
+    private JLabel dormNumberL;
+    private JLabel eligibilityL;
+    private JLabel departmentL;
+    private JLabel equipmentsL;
+    private JButton deallocateDormButton;
+    private JButton backButton;
 }
