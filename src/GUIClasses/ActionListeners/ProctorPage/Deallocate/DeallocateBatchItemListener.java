56e5705ba93db635777e028a3bd6777596edfdef
Yagth <yabuman46@gmail.com>
2022-09-04 05:55:42 -0700

    Added item listener

---
+++ src/GUIClasses/ActionListeners/ProctorPage/Deallocate/DeallocateBatchItemListener.java
@@ -0,0 +1,27 @@
+package GUIClasses.ActionListeners.ProctorPage.Deallocate;
+
+import GUIClasses.ProctorViews.DeallocateDormView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class DeallocateBatchItemListener implements ItemListener {
+    private DeallocateDormView parentComponent;
+    public DeallocateBatchItemListener(DeallocateDormView parentComponent){
+        this.parentComponent = parentComponent;
+    }
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        String selectedCondition = parentComponent.getSelectedCondition();
+        if(selectedCondition.equals("deallocate Batch of students")){
+            parentComponent.setYearTFVisibility(true);
+            parentComponent.setNumberOfStudentsL();
+        }
+        else if(selectedCondition.equals("deallocate non eligible")){
+            parentComponent.setYearTFVisibility(false);
+            parentComponent.setNumberOfStudentsL();
+        }
+    }
+}
--- src/GUIClasses/ProctorViews/DeallocateDormView.java
+++ src/GUIClasses/ProctorViews/DeallocateDormView.java
@@ -1,12 +1,16 @@
 package GUIClasses.ProctorViews;
 
+import BasicClasses.Others.JavaConnection;
 import BasicClasses.Persons.Proctor;
 import GUIClasses.ActionListeners.DeallocateBackButtonListener;
+import GUIClasses.ActionListeners.ProctorPage.Deallocate.DeallocateBatchItemListener;
 import GUIClasses.Interfaces.Views;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DeallocateDormView extends JFrame implements Views {
     private JFormattedTextField fromBuildingNoTF;
@@ -15,6 +19,7 @@ public class DeallocateDormView extends JFrame implements Views {
     private JButton backButton;
     private JButton deallocateButton;
     private JLabel numberOfStudentsL;
+    private JFormattedTextField yearTF;
     private Proctor proctor;
     private DormitoryView parentComponent;
     public DeallocateDormView(DormitoryView parentComponent, Proctor proctor){
@@ -23,6 +28,14 @@ public class DeallocateDormView extends JFrame implements Views {
         setUpGUi();
     }
 
+    public int getYear(){
+        try{
+            return Integer.parseInt(yearTF.getText());
+        } catch (NumberFormatException ex){
+            JOptionPane.showMessageDialog(this,"Please make sure there is a valid year input.");
+            return 0;
+        }
+    }
     public DeallocateDormView(){
         this(null,null);
     }//For debugging only constructor.
@@ -36,16 +49,31 @@ public class DeallocateDormView extends JFrame implements Views {
     public String getBuildingNo(){
         return fromBuildingNoTF.getText();
     }
-    public int getNoOfStudent(){
-        //This function will return the number of
-        // students that satisfy the condition selected in the condition's comboBox.
-        return 0;
+    public int getNoOfStudent(String query){
+        /*This function will return the number of
+         students that satisfy the condition selected
+          in the condition's comboBox.*/
+
+        JavaConnection javaConnection = new JavaConnection(JavaConnection.URL);
+        ResultSet resultSet = null;
+        int numberOfStudents = 0;
+        if(javaConnection.isConnected()){
+            resultSet = javaConnection.selectQuery(query);
+        }
+        try{
+            while(resultSet.next()){
+                numberOfStudents = resultSet.getInt("numberOfStudents"); // There will only be one row.
+            }
+        } catch (SQLException ex){
+            ex.printStackTrace(); // For debugging.
+        }
+        return numberOfStudents;
     }
     @Override
     public void setUpGUi() {
         this.setContentPane(mainPanel);
         this.setTitle("Deallocate Dorm");
-        this.setSize(500,300);
+        this.setSize(530,300);
         this.setLocationRelativeTo(parentComponent);
         backButton.addActionListener(new DeallocateBackButtonListener(this));
         this.addWindowListener(new WindowAdapter()
@@ -61,6 +89,27 @@ public class DeallocateDormView extends JFrame implements Views {
         this.setVisible(true);
         conditions.addItem("deallocate Batch of students");
         conditions.addItem("deallocate non eligible");
+        conditions.addItemListener(new DeallocateBatchItemListener(this));
+    }
 
+    public JLabel getNumberOfStudentsL(){
+        return numberOfStudentsL;
+    }
+    public void setYearTFVisibility(boolean visibility){
+        yearTF.setVisible(visibility);
+    }
+
+    public void setNumberOfStudentsL(){
+        String condition = getSelectedCondition();
+        if(condition.equals("deallocate Batch of students")){
+            String query = "SELECT COUNT(SID) AS numberOfStudents FROM STUDENT WHERE Year="+getYear();
+            numberOfStudentsL.setText(String.valueOf(getNoOfStudent(query)));
+            numberOfStudentsL.setVisible(true);
+        }
+        else if(condition.equals("deallocate non eligible")){
+            String query = "SELECT COUNT(SID) AS numberOfStudents FROM STUDENT WHERE isEligible="+0;
+            numberOfStudentsL.setText(String.valueOf(getNoOfStudent(query)));
+            numberOfStudentsL.setVisible(true);
+        }
     }
 }
