e246e3b904063ad24cfdda66464feae5cff2f72e
Yagth <yabuman46@gmail.com>
2022-09-07 07:07:18 -0700

    Added allocate dorm to students item listener

--- src/GUIClasses/ProctorViews/DormitoryView.java
+++ src/GUIClasses/ProctorViews/DormitoryView.java
@@ -2,6 +2,7 @@ package GUIClasses.ProctorViews;
 
 import BasicClasses.Enums.SizeOfMajorClasses;
 import BasicClasses.Persons.Proctor;
+import GUIClasses.ActionListeners.ProctorView.AllocateDormView.AllocateDormAsRequested;
 import GUIClasses.ActionListeners.ProctorView.DormitoryView.BackLabelListener;
 import GUIClasses.ActionListeners.ProctorView.DormitoryView.ChangeMenuListener;
 import GUIClasses.ActionListeners.ProctorView.DormitoryView.DeallocateMenuListener;
@@ -98,6 +99,7 @@ public class DormitoryView extends JFrame implements Views {
         JMenu actions = new JMenu("Actions");
 
         JMenuItem allocate = new JMenuItem("Allocate Dorms");
+        allocate.addActionListener(new AllocateDormAsRequested(this));
         JMenuItem deallocate = new JMenuItem("Deallocate all dorms");
         deallocate.addActionListener(new DeallocateMenuListener(this));
         JMenuItem change = new JMenuItem("Change Dorm");
