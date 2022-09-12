c76b99542c9dc5fe4e252da3c0ba625a7be10d85
Yagth <yabuman46@gmail.com>
2022-09-12 02:30:27 -0700

    Added a method to get request detail from database

--- src/GUIClasses/ActionListeners/ProctorView/ProctorPage/ReportDetailClickListener.java
+++ src/GUIClasses/ActionListeners/ProctorView/ProctorPage/ReportDetailClickListener.java
@@ -1,7 +1,7 @@
 package GUIClasses.ActionListeners.ProctorView.ProctorPage;
 
 import BasicClasses.Others.JavaConnection;
-import BasicClasses.Requests.Request;
+import BasicClasses.Requests.*;
 import GUIClasses.ProctorViews.ProctorPage;
 
 import javax.swing.*;
@@ -9,6 +9,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ReportDetailClickListener implements MouseListener {
     private ProctorPage parentComponent;
@@ -20,15 +21,55 @@ public class ReportDetailClickListener implements MouseListener {
     public Request getRequest(){
         JavaConnection javaConnection = new JavaConnection(JavaConnection.URL);
         ResultSet resultSet;
+
         JTable table = parentComponent.getReportTable();
         DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
         int clickedRow = table.getSelectedRow();
-        int ReportId = (int) tableModel.getValueAt(clickedRow,0);
-        String query;
+        int reportId = (int) tableModel.getValueAt(clickedRow,0);
+        String reportType = (String) tableModel.getValueAt(clickedRow,1);
+        Request request;
+
+        String query = "SELECT * FROM AllReports WHERE ReportId="+reportId+" AND ReportType='"+reportType+"'";
 
 
         if(javaConnection.isConnected()){
-
+            resultSet = javaConnection.selectQuery(query);
+            try{
+                while(resultSet.next()){
+                    switch (reportType){
+                        case "ClothTakeOutForm":
+                            request = new ClothTakeOutRequest(resultSet.getString("ReporterId"));
+                            request.setRequestedDate(resultSet.getDate("ReportedDate"));
+                            request.setHandledDate(resultSet.getDate("HandledDate"));
+                            request.setLocation(resultSet.getString("BuildingNumber")+" "+resultSet.getString("RoomNumber"));
+                            request.setDescription(resultSet.getString("Description"));
+                            return request;
+                        case "ExtendDormStayRequest":
+                            request = new ExtendDormStayRequest(resultSet.getString("ReporterId"));
+                            request.setRequestedDate(resultSet.getDate("ReportedDate"));
+                            request.setHandledDate(resultSet.getDate("HandledDate"));
+                            request.setLocation(resultSet.getString("BuildingNumber")+" "+resultSet.getString("RoomNumber"));
+                            request.setDescription(resultSet.getString("Description"));
+                            return request;
+                        case "Maintenance":
+                            request = new MaintenanceRequest(resultSet.getString("ReporterId"));
+                            request.setRequestedDate(resultSet.getDate("ReportedDate"));
+                            request.setHandledDate(resultSet.getDate("HandledDate"));
+                            request.setLocation(resultSet.getString("BuildingNumber")+" "+resultSet.getString("RoomNumber"));
+                            request.setDescription(resultSet.getString("Description"));
+                            return request;
+                        case "RequestForNewDorm":
+                            request = new RequestForNewDorm(resultSet.getString("ReporterId"));
+                            request.setRequestedDate(resultSet.getDate("ReportedDate"));
+                            request.setHandledDate(resultSet.getDate("HandledDate"));
+                            request.setLocation(resultSet.getString("BuildingNumber")+" "+resultSet.getString("RoomNumber"));
+                            request.setDescription(resultSet.getString("Description"));
+                            return request;
+                    }
+                }
+            } catch (SQLException ex){
+                ex.printStackTrace();//For debugging only.
+            }
         }
         return null;
     }
