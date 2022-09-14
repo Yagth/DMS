8b9ebb0026f83819001df577aa84c42bd0b381e7
Yagth <yabuman46@gmail.com>
2022-09-13 22:06:20 -0700

    Finished the filter button action listener

--- src/GUIClasses/ActionListeners/ProctorView/StudentView/FilterButtonListener.java
+++ src/GUIClasses/ActionListeners/ProctorView/StudentView/FilterButtonListener.java
@@ -1,11 +1,41 @@
 package GUIClasses.ActionListeners.ProctorView.StudentView;
 
+import GUIClasses.ProctorViews.StudentView;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 public class FilterButtonListener implements ActionListener {
+    private StudentView parentComponent;
+    public FilterButtonListener(StudentView parentComponent){
+        this.parentComponent = parentComponent;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
+        Vector<Vector<Object>> filteredStudents = filterStudents();
+        parentComponent.getTableData().clear();
+        parentComponent.addDataToTable(filteredStudents);
+        parentComponent.refreshTable();
+    }
 
+    public int getFilteringColumn(){
+        String selectedCondition = parentComponent.getSelectedCondition();
+        if(selectedCondition.equals("Year of students")) return 3; //This is the column index for the year in the table.
+        else if(selectedCondition.equals("Block")) return 4; //This is the column index for the buildingNumber in the table.
+        else return 5; //This is the column index for the eligibility in the table.
+    }
+
+    public Vector<Vector<Object>> filterStudents(){
+        Vector<Vector<Object>> students = parentComponent.getTableData();
+        Vector<Vector<Object>> tmp = new Vector<>();
+        Object comparingObject = parentComponent.getFilterInputText();
+        int index = getFilteringColumn();
+
+        for(Vector<Object> student : students){
+            if(student.get(index).equals(comparingObject))
+                tmp.add(student);
+        }
+        return tmp;
     }
 }
---
+++ src/GUIClasses/ActionListeners/ProctorView/StudentView/filterConditionItemListener.java
@@ -0,0 +1,11 @@
+package GUIClasses.ActionListeners.ProctorView.StudentView;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class filterConditionItemListener implements ItemListener {
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+}
