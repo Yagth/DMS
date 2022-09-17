466b422fc2a722f12350e4ad9a97afe813ef3dcc
Yagth <yabuman46@gmail.com>
2022-09-16 23:37:02 -0700

    Final Edit to the proctor page

---
+++ src/GUIClasses/ProctorViews/ProctorPage2.java
@@ -0,0 +1,20 @@
+package GUIClasses.ProctorViews;
+
+import javax.swing.*;
+
+public class ProctorPage2 {
+    private JPanel MainPanel;
+    private JPanel CentralPanel;
+    private JPanel SchedulePanel;
+    private JPanel ScheduleBodyPanel;
+    private JLabel blockNumberL;
+    private JLabel fromDateL;
+    private JLabel toDateL;
+    private JPanel ReportPanel;
+    private JScrollPane ReportScrollPane;
+    private JTable ReportTable;
+    private JPanel ReportsMTpanel;
+    private JPanel BottomPanel;
+    private JButton prevButton;
+    private JButton nextButton;
+}
