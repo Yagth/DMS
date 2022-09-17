cfb3c6c685b21e5e31cfaa8b53c4ccc04d4484bb
Yagth <yabuman46@gmail.com>
2022-09-17 03:50:37 -0700

    Created stock Menu item listener

---
+++ src/GUIClasses/ActionListeners/ProctorView/ProctorPage/StockMenuItemListener.java
@@ -0,0 +1,30 @@
+package GUIClasses.ActionListeners.ProctorView.ProctorPage;
+
+import BasicClasses.Persons.Proctor;
+import GUIClasses.ProctorViews.ProctorPage;
+import GUIClasses.ProctorViews.StockView;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class StockMenuItemListener extends MenuItemListener{
+    private ProctorPage parentComponent;
+
+    public StockMenuItemListener(ProctorPage parentComponent) {
+        super(parentComponent);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try{
+            Proctor proctor = parentComponent.getProctor();
+            parentComponent.setVisible(false);
+            new StockView(parentComponent,proctor);
+        } catch (NullPointerException ex){
+            ex.printStackTrace();//For debugging only.
+            JOptionPane.showMessageDialog(parentComponent,"Couldn't Open this page due to some error.",
+                    "Error opening page",JOptionPane.ERROR_MESSAGE);
+            parentComponent.setVisible(true);
+        }
+    }
+}
