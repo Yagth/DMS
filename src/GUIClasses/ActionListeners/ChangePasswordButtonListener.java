b79491b41ad71f1c2442d6d2c3fa4fe7fd23d297
Yagth <yabuman46@gmail.com>
2022-09-20 11:00:32 -0700

    Made the code to go back.

--- src/GUIClasses/ActionListeners/ChangePasswordButtonListener.java
+++ src/GUIClasses/ActionListeners/ChangePasswordButtonListener.java
@@ -31,8 +31,8 @@ public class ChangePasswordButtonListener implements ActionListener {
                 JOptionPane.showMessageDialog(parentComponent,"Password changed successfully.");
             else
                 JOptionPane.showMessageDialog(parentComponent,"Couldn't change password due to some reason.");
+            parentComponent.goBackToParent();
         }
-        parentComponent.goBackToParent();
     }
 
     public boolean changePassword(String newPassword){
