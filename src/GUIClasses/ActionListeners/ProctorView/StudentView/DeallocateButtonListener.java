6b6d34ffc1c7a81fdeeccca9766d1e8f713eb32b
Yagth <yabuman46@gmail.com>
2022-09-20 04:01:42 -0700

    Added inserting hisotory to the deallocation of a single student.

--- src/GUIClasses/ActionListeners/ProctorView/StudentView/DeallocateButtonListener.java
+++ src/GUIClasses/ActionListeners/ProctorView/StudentView/DeallocateButtonListener.java
@@ -1,6 +1,7 @@
 package GUIClasses.ActionListeners.ProctorView.StudentView;
 
 import BasicClasses.Others.JavaConnection;
+import BasicClasses.Requests.Request;
 import GUIClasses.ProctorViews.StudentDetailView;
 
 import javax.swing.*;
@@ -19,6 +20,10 @@ public class DeallocateButtonListener implements ActionListener {
         if(choice == 1) return;
         else{
             boolean updateStatus = deallocateStudent();
+            String query = "INSERT INTO ProctorControlsStock(EID,ActionType,ActionDate,BuildingNumber) "+
+                    " VALUES('"+parentComponent.getProctor().getpId()+"' , 'Allocate Dorm', '"+
+                    Request.getCurrentDate()+"' , '"+parentComponent.getProctor().getBuildingNo()+"')";
+            insertHistory(query);
             displayUpdateStatus(updateStatus);
             parentComponent.reloadParentTable();
             parentComponent.goBackToParent();
@@ -26,15 +31,27 @@ public class DeallocateButtonListener implements ActionListener {
     }
 
     public boolean deallocateStudent(){
+        String buildingNumber = String.valueOf(parentComponent.getStudent().getBuildingNo());
         JavaConnection javaConnection = new JavaConnection(JavaConnection.URL);
-        String query = "UPDATE Student SET buildingNumber=null, roomNumber=null WHERE SID='"+parentComponent.getStudentID()+"'";
+        String query = "UPDATE Student SET buildingNumber=null, roomNumber=null,pillow=0,matress=0,bedBase=0 WHERE SID='"+parentComponent.getStudentID()+"'";
         boolean updateStatus = false;
 
-        if(javaConnection.isConnected()) updateStatus = javaConnection.updateQuery(query);
+        if(javaConnection.isConnected()){
+            updateStatus = javaConnection.updateQuery(query);
+            query = "UPDATE Stock SET TotalPillow+=1, TotalMatress+=1," +
+                    " TotalMatressBase+=1 WHERE BuildingNumber='"+buildingNumber+"';";
+            updateStatus &= javaConnection.updateQuery(query);
+        }
 
         return updateStatus;
     }
 
+    public void insertHistory(String query){
+        JavaConnection javaConnection = new JavaConnection(JavaConnection.URL);
+        if(javaConnection.isConnected()){
+            javaConnection.insertQuery(query);
+        }
+    }
     public void displayUpdateStatus(boolean updateStatus){
         if(updateStatus) JOptionPane.showMessageDialog(parentComponent,"Deallocation successful");
         else JOptionPane.showMessageDialog(parentComponent,"Couldn't perform operation");
