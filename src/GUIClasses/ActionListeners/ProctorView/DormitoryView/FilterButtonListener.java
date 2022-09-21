8c3328f1245800043240971cfed30d7d82449344
Yagth <yabuman46@gmail.com>
2022-09-21 04:29:13 -0700

    Removed the order by for the filter condition null.

--- src/GUIClasses/ActionListeners/ProctorView/DormitoryView/FilterButtonListener.java
+++ src/GUIClasses/ActionListeners/ProctorView/DormitoryView/FilterButtonListener.java
@@ -71,7 +71,7 @@ public class FilterButtonListener implements ActionListener {
                     " ROWS FETCH NEXT "+parentComponent.getRowPerPage()+" ROWS ONLY;";
         } else{
             query = "SELECT BuildingNumber, RoomNumber, maxCapacity, NumberOfStudents " +
-                    "FROM AvailableDorm ORDER BY NumberOfStudents"+
+                    "FROM AvailableDorm "+
                     " OFFSET "+(parentComponent.getPageNumber()-1)* parentComponent.getRowPerPage()+
                     " ROWS FETCH NEXT "+parentComponent.getRowPerPage()+" ROWS ONLY;";
         }
